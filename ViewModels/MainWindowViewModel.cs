ffaec51009db44b94260e17a47983e506512a6ce
Farzad <farzad.rabiee@outlook.com>
2022-08-06 13:48:08 +1000

    First Commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Input;
+using Avalonia.Controls;
+using ReactiveUI;
+
+namespace Avalonia.NETCoreMVVMApp1.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private int count;
+        public int Count
+        {
+            get => count; 
+            set => this.RaiseAndSetIfChanged(ref count, value);
+        }
+        public ICommand EventClick { get; private set; }
+        public MainWindowViewModel()
+        {
+            var ofd = new OpenFileDialog();
+            EventClick = ReactiveCommand.Create(() =>
+            {
+                Console.WriteLine("Click!");
+            });
+        }
+    }
+}
